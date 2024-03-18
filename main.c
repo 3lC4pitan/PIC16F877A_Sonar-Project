ae4c681d512807166b048cfebe2e693c9dae3a26
Amine NAOUR <15603323+3lC4pitan@users.noreply.github.com>
2024-03-18 02:29:32 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: nmeda
+ *
+ * Created on March 18, 2024, 2:15 AM
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include <pic16f877a.h>
+
+void main(void) {
+    return;
+}
